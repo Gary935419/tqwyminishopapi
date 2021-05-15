ffdf6e3920dfa1d25312b9e4bc8f9983537417d9
Gary <zhaoyue@dlhzxt.cn>
2021-05-15 11:45:14 +0800

    login

--- src/plugins/wxapp/models/LoginForm.php
+++ src/plugins/wxapp/models/LoginForm.php
@@ -26,47 +26,35 @@ class LoginForm extends \app\forms\api\LoginForm
         /** @var Plugin $plugin */
         $plugin = new Plugin();
         $postData = \Yii::$app->request->post();
-
+        
         if (isset($postData['rawData'])) {
             $rawData = $postData['rawData'];
             $postUserInfo = json_decode($rawData, true);
-            $data = $plugin->getWechat()->decryptData(
-                $postData['encryptedData'],
-                $postData['iv'],
-                $postData['code']
-            );
-
-            $nickName = $postUserInfo['nickName'];
-            $avatarUrl = $postUserInfo['avatarUrl'];
-            $gender = $postUserInfo['gender'];
-            $language = $postUserInfo['language'];
-            $city = $postUserInfo['city'];
-            $province = $postUserInfo['province'];
-            $country = $postUserInfo['country'];
-            $openId = $data['openId'];
+//            $data = $plugin->getWechat()->decryptData(
+//                $postData['encryptedData'],
+//                $postData['iv'],
+//                $postData['code']
+//            );
+            $data = $plugin->getWechat()->jsCodeToSession($postData['code']);
+            $openId = $data['openid'];
             $unionId = $data['unionId'] ? $data['unionId'] : '';
         } else {
             $scope = 'auth_base';
             $data = $plugin->getWechat()->jsCodeToSession($postData['code']);
-            $nickName = $data['nickName'];
-            $avatarUrl = $data['avatarUrl'];
             $openId = $data['openid'];
             $unionId = $data['unionId'] ? $data['unionId'] : '';
             $postUserInfo['nickName'] = '';
             $postUserInfo['avatarUrl'] = '';
         }
-       
         $userInfo = new LoginUserInfo();
         $userInfo->username = $openId;
         $userInfo->scope = $scope;
         $userInfo->nickname = isset($data['nickName']) ? $data['nickName'] : $postUserInfo['nickName'];
         $userInfo->avatar = isset($data['avatarUrl']) ? $data['avatarUrl'] : $postUserInfo['avatarUrl'];
-        // $userInfo->nickname = isset($nickName) ? $nickName : $postUserInfo['nickName'];
-        // $userInfo->avatar = isset($avatarUrl) ? $avatarUrl : $postUserInfo['avatarUrl'];
         $userInfo->platform_user_id = $openId;
         $userInfo->platform = UserInfo::PLATFORM_WXAPP;
         $userInfo->unionId = $unionId;
-        
+
         return $userInfo;
     }
 }
